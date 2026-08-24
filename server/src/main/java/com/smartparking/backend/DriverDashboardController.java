8dfdb59af4318351e5746574c999141e4b984494
sasjess <sanilijesmina@gmail.com>
2026-08-24 14:55:37 +0530

    implement driver dashboard UI

---
+++ server/src/main/java/com/smartparking/backend/DriverDashboardController.java
@@ -0,0 +1,13 @@
+package com.smartparking.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DriverDashboardController {
+
+    @GetMapping("/driver")
+    public String driverDashboard() {
+        return "driver-dashboard";
+    }
+}
